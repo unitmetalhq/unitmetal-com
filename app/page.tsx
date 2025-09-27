dd4413a261f4d389c998b67a0e412a1cde7b661c
ZxStim <zxstim@gmail.com>
2025-09-27 17:59:59 +0700

    feat: added contents

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,67 @@
-import Image from "next/image";
+import TerminalMenu from "@/components/terminal-menu";
+import KeyboardNav from "@/components/keyboard-nav";
 
 export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+  const menuItems = [
+    {
+      id: 1,
+      name: 'Go to app',
+      url: '/app'
+    },
+    {
+      id: 2,
+      name: 'Explore yield',
+      url: '/explore'
+    },
+    {
+      id: 3,
+      name: 'Learn more about DeFi',
+      url: '/learn'
+    },
+  ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+  const keyboardNavItems = [
+    {
+      keyboard: '1',
+      description: 'Sections',
+      url: '/app'
+    },
+    {
+      keyboard: '2',
+      description: 'Sections',
+      url: '/explore'
+    },
+    {
+      keyboard: '3',
+      description: 'Sections',
+      url: '/learn'
+    },
+  ];
+
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-0">
+      <div className="flex flex-col gap-4 w-full md:w-5/6 text-left">
+        <div className="flex flex-row gap-2 items-center">
+          <span className="text-sm bg-primary text-secondary font-bold px-2 py-1">00.</span>
+          <h1 className="text-md font-bold">Index</h1>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <h1 className="text-xl md:text-3xl font-bold">Professional Interface for DeFi</h1>
+        <div className="flex flex-col gap-12 mt-4">
+          <p className="text-md">High quality, open source, local first interface for DeFi users. Swap, provide liquidity and explore yield opportunities.</p>
+          <p className="text-md">Founded upon the freedom software movement ethos of transparency, collaboration, and empowerment.</p>
+        </div>
+      </div>
+      <div className="flex flex-col border-2 border-primary gap-2 pb-8">
+        <div className="flex flex-row justify-between items-center bg-primary text-secondary p-2">
+          <h1 className="text-lg md:text-xl font-bold">Terminal</h1>
+          <p className="text-md">_</p>
+        </div>
+        <div className="flex flex-col px-4 py-2">
+          <h2 className="text-md">$ navigate</h2>
+          <TerminalMenu menuItems={menuItems} />
+        </div>
+      </div>
+      <KeyboardNav keyboardNavItems={keyboardNavItems} />
     </div>
   );
 }
---
+++ components/site-header.tsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import Image from "next/image";
+import ThemeToggle from "@/components/theme-toggle";
+export default function SiteHeader() {
+  return (
+    <div className="flex flex-col gap-2 md:flex-row items-center justify-between w-full">
+      <Link href="/">
+        <Image
+          src="/unitmetal-full.svg"
+          alt="UnitMetal Full Logo"
+          width={1200}
+          height={200}
+          className="max-w-48 dark:invert"
+          />
+      </Link>
+      <div className="flex flex-row gap-2">
+        <ThemeToggle />
+      </div>
+    </div>
+  )
+}
