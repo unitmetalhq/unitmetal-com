0a2b80146f0466e74d709b204c65f1475410cbf8
ZxStim <zxstim@gmail.com>
2025-09-29 21:42:04 +0700

    fix: more fixes

--- next.config.ts
+++ next.config.ts
@@ -9,18 +9,6 @@ const nextConfig: NextConfig = {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
     return config;
   },
-  turbopack: {
-    rules: {
-      '*.node': {
-        loaders: ['external-loader'],
-      },
-    },
-    resolveAlias: {
-      'pino-pretty': '',
-      'lokijs': '',
-      'encoding': '',
-    },
-  }
 };
 
 export default nextConfig;
